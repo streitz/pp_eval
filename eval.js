0e372e05cb311a49300ecc56201be36e357f95c1
Adrian Streitz <a.streitz@gmail.com>
2013-07-12 16:23:48 +0200

    Layout changes

--- eval.js
+++ eval.js
@@ -5,6 +5,17 @@
 	    fade: 250
         })
 
+        var features_pattern;
+        var features_db;
+
+        $.get("data/pattern.csv", function(data){
+            features_pattern = processData(data);
+        })
+
+        // $.get("data/db.csv", function(data){
+        //     features_db = processData(data);
+        // })
+
         var data = {
             availability: 0,
             consistency: 0,
@@ -12,23 +23,38 @@
             size: 0,
             growth: 0,
             frequency: 0,
-            f_change: 0
+            f_change: 0,
+            innovation: 0,
+            investment: 0,
+            general: 0,
+            requirements: 0,
+            data: 0,
+            data_structures: 0,
+            data_properties: 0,
+            query: 0,
+            query_types: 0
         }
 
-        var data_types = {
+        var data_structures = {
             relat: 0,
+            graph: 0,
+            hier: 0,
+            doc: 0,
+            kv: 0
+        }
+
+        var data_categories = {
             trans: 0,
             mass: 0,
             bulk: 0,
-            time: 0,
-            hier: 0,
-            graph: 0
+            time: 0
         }
 
         var query_types = {
             id: 0,
             example: 0,
-            relation: 0
+            relation: 0,
+            fulltext: 0
         }
 
         var total = function (model) {
@@ -42,18 +68,26 @@
             return sum;
         }
 
+        var features_db;
+        var features_pattern;
+
+
         var update = function () {
-            $("#relational-value").html(Math.floor(data_types.relat*100/total(data_types)));
-            $("#transient-value").html(Math.floor(data_types.trans*100/total(data_types)));
-            $("#mass-value").html(Math.floor(data_types.mass*100/total(data_types)));
-            $("#bulk-value").html(Math.floor(data_types.bulk*100/total(data_types)));
-            $("#time-value").html(Math.floor(data_types.time*100/total(data_types)));
-            $("#hierarchical-value").html(Math.floor(data_types.hier*100/total(data_types)));
-            $("#graph-value").html(Math.floor(data_types.graph*100/total(data_types)));
+            $("#relational-value").html(Math.floor(data_structures.relat*100/total(data_structures)));
+            $("#graph-value").html(Math.floor(data_structures.graph*100/total(data_structures)));
+            $("#hierarchical-value").html(Math.floor(data_structures.hier*100/total(data_structures)));
+            $("#document-value").html(Math.floor(data_structures.doc*100/total(data_structures)));
+            $("#kv-value").html(Math.floor(data_structures.kv*100/total(data_structures)));
+
+            $("#transient-value").html(Math.floor(data_categories.trans*100/total(data_categories)));
+            $("#mass-value").html(Math.floor(data_categories.mass*100/total(data_categories)));
+            $("#bulk-value").html(Math.floor(data_categories.bulk*100/total(data_categories)));
+            $("#time-value").html(Math.floor(data_categories.time*100/total(data_categories)));
 
             $("#query_type_id-value").html(Math.floor(query_types.id*100/total(query_types)));
             $("#query_type_example-value").html(Math.floor(query_types.example*100/total(query_types)));
             $("#query_type_relationship-value").html(Math.floor(query_types.relation*100/total(query_types)));
+            $("#query_type_fulltext-value").html(Math.floor(query_types.fulltext*100/total(query_types)));
         }
 
         var normalSlider = function (id, model, field) {
@@ -71,17 +105,21 @@
             }) 
         }
 
-        normalSlider('#slider-relational', data_types, 'relat')
-        normalSlider('#slider-transient', data_types, 'trans')
-        normalSlider('#slider-mass', data_types, 'mass')
-        normalSlider('#slider-bulk', data_types, 'bulk')
-        normalSlider('#slider-time', data_types, 'time')
-        normalSlider('#slider-hierarchical', data_types, 'hier')
-        normalSlider('#slider-graph', data_types, 'graph')
+        normalSlider('#slider-relational', data_structures, 'relat')
+        normalSlider('#slider-hierarchical', data_structures, 'hier')
+        normalSlider('#slider-graph', data_structures, 'graph')
+        normalSlider('#slider-kv', data_structures, 'kv')
+        normalSlider('#slider-document', data_structures, 'doc')
+
+        normalSlider('#slider-transient', data_categories, 'trans')
+        normalSlider('#slider-mass', data_categories, 'mass')
+        normalSlider('#slider-bulk', data_categories, 'bulk')
+        normalSlider('#slider-time', data_categories, 'time')
 
         normalSlider('#slider-query_type_id', query_types, 'id')
         normalSlider('#slider-query_type_example', query_types, 'example')
         normalSlider('#slider-query_type_relationship', query_types, 'relation')
+        normalSlider('#slider-query_type_fulltext', query_types, 'fulltext')
 
         var labeledSlider = function (id, field, labels) {
             $(id).labeledslider({
@@ -101,15 +139,39 @@
 
         var changes = ['none', 'marg', 'lin', 'poly', 'exp']
         var categories = ['not nec', 'not imp', 'no matter', 'imp', 'very imp']
+        var categories2 = ['low', 'medium', 'high']
 
         labeledSlider('#slider-availability', 'availability', categories)
         labeledSlider('#slider-consistency', 'consistency', categories)
         labeledSlider('#slider-ptolerance', 'ptolerance', categories)
-        labeledSlider('#slider-data_sizing', 'size', ['kB', 'MB', 'GB', 'TB', 'PB'])
+        labeledSlider('#slider-data_sizing', 'size', ['< 100 MB', '< 1 GB', '< 100 GB', '< 1 TB', 'TB', 'PB'])
         labeledSlider('#slider-data_growth', 'growth', changes)
         labeledSlider('#slider-query_freq', 'frequency', ['<= 1', '10', '100', '1000', '10000', '>=100000'])
         labeledSlider('#slider-query_prediction', 'f_change', changes)
+        labeledSlider('#slider-innovation', 'innovation', categories2)
+        labeledSlider('#slider-investment', 'investment', categories2)
+
+        labeledSlider('#slider-general', 'general', categories2)
+        labeledSlider('#slider-requirements', 'requirements', categories2)
+        labeledSlider('#slider-data', 'data', categories2)
+        labeledSlider('#slider-data_type', 'data_type', categories2)
+        labeledSlider('#slider-data_properties', 'data_properties', categories2)
+        labeledSlider('#slider-query', 'query', categories2)
+        labeledSlider('#slider-query_types', 'query_types', categories2)
+
 
         update();
     });
+
+    function processData(content) {
+        var contentLines = content.split(/\r\n|\n/);
+        var lines = [];
+
+        for (var i=0; i<contentLines.length; i++) {
+            var data = contentLines[i].split(';');
+            lines.push(data);
+        }
+
+        return lines;
+    }
 })(jQuery);
